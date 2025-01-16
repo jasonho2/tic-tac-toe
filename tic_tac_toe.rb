7bfe7e2e8feecb4e85ed965cca80a9bf1d68e8d9
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-01-15 21:30:58 -0800

    Finish methods to complete the game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,6 +5,18 @@ class TicTacToe
     @current_player = 'X'
   end
 
+  def play
+    display_board
+    until game_over?
+      player_turn
+      display_board
+      break if winner
+      switch_player
+    end
+    display_board
+    announce_result
+  end
+
   private
 
   def display_board
@@ -15,7 +27,64 @@ class TicTacToe
     end
   end
 
+  def player_turn
+    puts "Player #{@current_player}, enter your move (row and column: 1 1 for middle):"
+    row, col = gets.chomp.split.map(&:to_i)
+
+    if valid_move?(row, col)
+      @board[row][col] = @current_player
+    else
+      puts "Invalid move! Try again."
+      player_turn
+    end
+  end
+
+  def valid_move?(row, col)
+    row.between?(0, 2) && col.between?(0, 2) && @board[row][col] == ' '
+  end
+
+  def switch_player
+    @current_player = @current_player == 'X' ? 'O' : 'X'
+  end
+
+  def game_over?
+    winner || draw?
+  end
+
+  def winner
+    lines = rows + columns + diagonals
+    lines.each do |line|
+      return @current_player if line.uniq.size == 1 && line.first != ' '
+    end
+    nil
+  end
+
+  def draw?
+    @board.flatten.none? { |cell| cell == ' ' }
+  end
+
+  def rows
+    @board
+  end
+
+  def columns
+    @board.transpose
+  end
+
+  def diagonals
+    [[@board[0][0], @board[1][1], @board[2][2]], [@board[0][2], @board[1][1], @board[2][0]]]
+  end
+
+  def announce_result
+    if winner
+      puts "Player #{winner} wins!"
+    else
+      puts "It's a draw!"
+    end
+  end
+
 end
 
 # Start the game
 game = TicTacToe.new
+game.play
